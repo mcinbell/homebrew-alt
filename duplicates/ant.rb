c88fef1493411d679a5517442d8f0f1178438a42
Egils Mūzis <egils.muzis@gmail.com>
2012-04-16 14:28:22 +0200

    Update duplicates/ant.rb

--- duplicates/ant.rb
+++ duplicates/ant.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Ant < Formula
-  url 'http://www.apache.org/dist/ant/binaries/apache-ant-1.8.2-bin.tar.gz'
+  url 'http://www.apache.org/dist/ant/binaries/apache-ant-1.8.3-bin.tar.gz'
   homepage 'http://ant.apache.org/'
   md5 'afb0c7950a663f94e65da9f3be676d8f'
 
