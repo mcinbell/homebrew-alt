4ad50bf8088650584bb83db7f655d9e8500d8670
Mike Bell <mmbell@gmail.com>
2012-07-04 15:12:49 +0200

    Updated apache ant version

--- duplicates/ant.rb
+++ duplicates/ant.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Ant < Formula
-  url 'http://www.apache.org/dist/ant/binaries/apache-ant-1.8.3-bin.tar.gz'
+  url 'http://www.apache.org/dist/ant/binaries/apache-ant-1.8.4-bin.tar.gz'
   homepage 'http://ant.apache.org/'
   md5 'd13be81f715c51ce3e13a7149fd92765'
 
