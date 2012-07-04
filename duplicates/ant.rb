96498ae5fb5592fc5e9781a5ad00b5f4d85defe0
Mike Bell <mmbell@gmail.com>
2012-07-04 15:28:28 +0200

    Updated md5

--- duplicates/ant.rb
+++ duplicates/ant.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ant < Formula
   url 'http://www.apache.org/dist/ant/binaries/apache-ant-1.8.4-bin.tar.gz'
   homepage 'http://ant.apache.org/'
-  md5 'd13be81f715c51ce3e13a7149fd92765'
+  md5 'f5975145d90efbbafdcabece600f716b'
 
   def install
     prefix.install Dir['*']
